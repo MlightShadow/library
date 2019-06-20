3dad93b6e39220220d6bd53bfce0f81309712d0e
MlightShadow <rwayxu@hotmail.com>
2019-06-20 17:35:34 +0800

    update

--- notes/ai/kNN.py
+++ notes/ai/kNN.py
@@ -4,6 +4,32 @@ import operator
 def log(label, var):
     print(label, var)
 
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOlines=fr.readlines()
+    numberOfLines = len(arrayOlines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)  
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))
+    return normDataSet, ranges, minVals
+
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
